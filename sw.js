d07c15d1786c2832f62d240cf5289ef3c608de1f
0nj52508385596j-ux <0nj52508385596j@gmail.com>
2026-07-16 14:36:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'reform-hub-v1-0-0';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/logo.svg','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
+const CACHE_NAME = 'reform-hub-v1-1-0-0';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
